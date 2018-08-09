1be954409cac96afc373eed7a01e6d03bf75b750
Abdul <abdul.kawee@evampsaanga.com>
2018-08-09 17:54:20 +0500

    Completed alert dialog

---
+++ app/src/main/java/com/learning/tipcalculator/tipcalculator/model/TipCalculationRepository.kt
@@ -0,0 +1,23 @@
+package com.learning.tipcalculator.tipcalculator.model
+
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+
+class TipCalculationRepository {
+
+    private val savedTips = mutableMapOf<String, TipCalculation>()
+
+    fun saveTipCalculation(tip : TipCalculation){
+        savedTips[tip.locationName] = tip
+    }
+
+    fun loadTipCalculationByName(locationName : String) : TipCalculation?{
+        return savedTips[locationName]
+    }
+
+    fun loadSavedTipCalculations(): LiveData<List<TipCalculation>> {
+        val liveData = MutableLiveData<List<TipCalculation>>()
+        liveData.value = savedTips.values.toList()
+        return liveData
+    }
+}
